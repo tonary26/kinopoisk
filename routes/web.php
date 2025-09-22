7c34e3bc7e2985663e551e91e8a52e7b958b5fa9
tonary26 <mpclown5@gmail.com>
2025-09-22 17:17:01 +0300

    routes commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\FilmController;
+
+
+Route::get('/', [FilmController::class, 'films'])->name('film.index');
+
+Route::get('/add', [FilmController::class, 'add'])->name('film.add');
+Route::post('/add/shop', [FilmController::class, 'addShop'])->name('film.addShop');
+
+Route::get('/show/{film}', [FilmController::class, 'show'])->name('film.get');
+
+Route::get('/{film}/update', [FilmController::class, 'update'])->name('film.update');
+Route::patch('/{film}/edit', [FilmController::class, 'edit'])->name('film.edit');
+
+Route::get('/delete/{film}', [FilmController::class, 'delete'])->name('film.delete');
