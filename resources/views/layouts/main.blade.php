ffd594e88e4477ba7c68c4f63ca43429f79c81cd
tonary26 <mpclown5@gmail.com>
2025-09-22 17:16:24 +0300

    layout commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,16 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/main.css'])
     @yield('styles')
-    <title>Магазин-продуктов</title>
+    <title>КиноПоиск</title>
 </head>
 <body>
 <div class="header">
-    <h1 class="logo">Кинопоиск</h1>
+    <a href="{{ route('film.index')  }}"> <h1 class="logo">Кинопоиск</h1> </a>
     <div class="nav-menu">
-        <a href="#">Фильмы</a>
-        <a href="#">Добавить фильм</a>
+        <a href="{{ route('film.index') }}">Фильмы</a>
+        <a href="{{ route('film.add') }}">Добавить фильм</a>
     </div>
     <div class="nav-auth">
         <a href="#">Login</a>
